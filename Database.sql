b9571fce490dc61383745c1875866e2802474fdf
diyaapatell <diyaapatell21@gmail.com>
2026-09-15 21:26:12 +0530

    Update Database.sql

--- Database.sql
+++ Database.sql
@@ -1,1 +1,34 @@
+CREATE DATABASE ecommerce_analysis;
 
+USE ecommerce_analysis;
+
+-- Customers table
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(100),
+    city VARCHAR(50)
+);
+
+-- Products table
+CREATE TABLE products (
+    product_id INT PRIMARY KEY,
+    product_name VARCHAR(100),
+    category VARCHAR(50),
+    price DECIMAL(10,2)
+);
+
+-- Orders table
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+-- Order Items table
+CREATE TABLE order_items (
+    order_id INT,
+    product_id INT,
+    quantity INT,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id)
+);
